e98fac322dd19cab39947192749ffc29648b4b7c
deplanty <yvon.deplante@gmail.com>
2019-11-13 13:52:12 +0100

    Modify mainwindow style

--- src/main/python/frames/ui/mainwindow.py
+++ src/main/python/frames/ui/mainwindow.py
@@ -27,30 +27,29 @@ class Ui_MainWindow():
         # Main frame
         frame = QWidget()
         frame.setObjectName("mainwindow")
+        self.v_layout = QVBoxLayout(frame)
         # Button create vr3 model
         self.btn_model_vr3 = QToolButton()
         self.btn_model_vr3.setIcon(QIcon(":/images/model-vr3.svg"))
         self.btn_model_vr3.setIconSize(QSize(32, 32))
         self.btn_model_vr3.setObjectName("tool")
         self.btn_model_vr3.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.v_layout.addWidget(self.btn_model_vr3)
         # Create script
         self.btn_script = QToolButton()
         self.btn_script.setIcon(QIcon(":/images/script.svg"))
         self.btn_script.setIconSize(QSize(32, 32))
         self.btn_script.setObjectName("tool")
         self.btn_script.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.v_layout.addWidget(self.btn_script)
         # Create dashboard
         self.btn_dashboard = QToolButton()
         self.btn_dashboard.setIcon(QIcon(":/images/dashboard.svg"))
         self.btn_dashboard.setIconSize(QSize(32, 32))
         self.btn_dashboard.setObjectName("tool")
         self.btn_dashboard.setCursor(Qt.CursorShape.PointingHandCursor)
-        # Set the buttons in a vertical layout
-        self.h_layout = QVBoxLayout(frame)
-        self.h_layout.addWidget(self.btn_model_vr3)
-        self.h_layout.addWidget(self.btn_script)
-        self.h_layout.addWidget(self.btn_dashboard)
-        # Window fit the layout
+        self.v_layout.addWidget(self.btn_dashboard)
+        # # Window fit the layout
         MainWindow.setCentralWidget(frame)
         MainWindow.setFixedSize(frame.sizeHint())
 
