faec5224d37d945510526d373b6a5cbc7d3291a4
deplanty <yvon.deplante@gmail.com>
2019-11-14 15:47:04 +0100

    Set qt resources in the source folder

--- convert_qrc.py
+++ convert_qrc.py
@@ -4,6 +4,6 @@ import os
 dir_this = os.path.dirname(__file__)
 rcc = os.path.join(dir_this, "venv\\Scripts\\pyside2-rcc.exe")
 qrc = os.path.join(dir_this, "resources\\resources.qrc")
-py = os.path.join(dir_this, "resources.py")
+py = os.path.join(dir_this, "src\\common\\resources.py")
 cmd = f"{rcc} {qrc} -o {py}"
 os.system(cmd)
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import sys
 
-from PySide2.QtCore import (
-    QFile
-)
-from PySide2.QtWidgets import (
-    QApplication
-)
+from PySide2.QtCore import QFile
+from PySide2.QtWidgets import QApplication
 
-import resources
+import src.common.resources
 from src.config import Paths
 from src.frames.controller import MainWindow
 
@@ -17,15 +13,16 @@ class Application(QApplication):
         QApplication.__init__(self, argv)
 
         Paths.initialize("resources/config/paths.json")
+        self.load_style()
+        self.window = MainWindow()
 
+    def load_style(self):
         file = QFile(":/styles/style.qss")
         file.open(QFile.ReadOnly)
         style = file.readAll()
         style = style.data().decode()
         file.close()
         self.setStyleSheet(style)
-
-        self.window = MainWindow()
 
     def run(self):
         self.window.show()
