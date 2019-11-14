654ba12ebbbdb6abee1e7e2ba73182edd9c8db20
deplanty <yvon.deplante@gmail.com>
2019-11-14 16:57:57 +0100

    Write docstrings and comments for main.py

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ import sys
 from PySide2.QtCore import QFile
 from PySide2.QtWidgets import QApplication
 
-import src.common.resources
+import src.common.resources  # Register resources
 from src.config import Paths
 from src.frames.controller import MainWindow
 
 
 class Application(QApplication):
+    """
+    Application for the ASL Toolbox
+    """
+
     def __init__(self, argv:list):
         QApplication.__init__(self, argv)
-
-        Paths.initialize("resources/config/paths.json")
+        # Load configs and styles
+        self.load_configs()
         self.load_style()
+        # Create the mainwindow
         self.window = MainWindow()
 
     def load_style(self):
+        """
+        The style is a css-like file
+        """
+
         file = QFile(":/styles/style.qss")
         file.open(QFile.ReadOnly)
         style = file.readAll()
@@ -24,7 +33,21 @@ class Application(QApplication):
         file.close()
         self.setStyleSheet(style)
 
+    def load_configs(self):
+        """
+        Initialize the configs
+        """
+
+        Paths.initialize("resources/config/paths.json")
+
     def run(self):
+        """
+        Main method to run the application
+
+        Returns:
+            int: exit code
+        """
+
         self.window.show()
         return self.exec_()
 
